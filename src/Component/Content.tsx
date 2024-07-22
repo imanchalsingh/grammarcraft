6b0f9a8b12e331e47e60a36771db93ac269d0790
anchalDev159357 <singhanchal159357@gmail.com>
2024-07-22 16:48:13 +0530

    added changes

--- src/Component/Content.tsx
+++ src/Component/Content.tsx
@@ -56,6 +56,7 @@ export default function Content() {
             >
               <h1 className="topic-heading">{items.topic}</h1>
             </div>
+            
           );
         })}
       </div>
